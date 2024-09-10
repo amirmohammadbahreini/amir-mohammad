3b54a3450e3d5ce0e42dbf27e56a84190400baaa
amirmohammadbahreini <amirarmaneh@gmail.com>
2024-09-10 16:17:12 +0330

    tamrin 1

---
+++ tamrin.py
@@ -0,0 +1,54 @@
+#1
+txt = "hello Amir\
+    I'm good"
+
+print(txt)
+
+#2
+
+txt2 = "Hello world"
+
+print(txt2.upper())
+
+#3
+
+str = "program"
+
+print(str[::3])
+
+#4
+
+str1 = "JhonDipPeta"
+str2 = "JaSonAy"
+
+print(str1[4:7])
+print(str2[2:5])
+
+#5
+
+s1 = 'Ault'
+s2 = 'Kelly'
+s3 = s1[:2] + s2 + s1[2:]
+
+print(s3)
+
+#6
+
+del s1 , s2 , s3
+
+s1 = "America"
+s2 = "Japan"
+s3 = s1[0]+s2[0]+s1[3]+s2[2]+s1[-1]+s2[-1]
+
+print(s3)
+
+#7  ?!
+
+#8
+
+U = "United"
+S = "states of"
+A = "America"
+USA = U+" "+S+" "+A
+
+print(USA)
